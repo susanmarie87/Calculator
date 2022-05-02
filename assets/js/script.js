282af99baf3c2c3c4cbbd7d5a20bdde49c150c9f
susanmarie87 <susan.marie87@gmail.com>
2022-05-02 02:57:52 +0000

    Setup function structure of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("connected")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function IncrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplicationQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
