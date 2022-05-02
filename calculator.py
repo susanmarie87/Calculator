44b67dd1b0726568599d5b7d6e3864f9518c6a44
susanmarie87 <susan.marie87@gmail.com>
2022-05-02 02:19:13 +0000

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,27 @@
+
+print("1 Addition")
+print("2 Subtraction")
+print("3 Multiplication")
+print("4 Division")
+
+choice = input("Enter number here: ")
+
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
+if choice == "1":
+    print(num1, "+", num2, "=", (num1+num2))
+elif choice == "2":
+    print(num1, "-", num2, "=", (num1-num2))
+elif choice == "3":
+    print(num1, "*", num2, "=", (num1*num2))
+elif choice == "4":
+    if num2 == 0.0:
+        print("Error: Cannot divide by 0")
+    else:
+        print(num1, "/", num2, "+", (num1/num2))
+
+else:
+    print("invalid choice")
+
+
